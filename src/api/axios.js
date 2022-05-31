31a3bb35dbffe624f0ec855065e4ce56fe75a28e
Vutuanhai3007 <vutuanhai30072002@gmail.com>
2022-05-31 15:06:05 +0700

    axios

---
+++ src/api/axios.js
@@ -0,0 +1,9 @@
+// Nơi cấu hình base cho axios
+import axios from 'axios';
+
+// Khởi tạo cấu hình axios cho toàn bộ project
+const apiAxios = axios.create({
+    baseURL: 'https://6295a27b810c00c1cb648974.mockapi.io'
+});
+
+export default apiAxios;
